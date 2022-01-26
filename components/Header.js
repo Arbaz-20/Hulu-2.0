9788544636e849a05ecefa3263b76f3647a33dde
Arbaz Khan <khanarbaz26393@gmail.com>
2022-01-26 20:32:08 +0530

    initial-commit

---
+++ components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Image from 'next/image';
+
+function Header() {
+  return( 
+    <header>
+        <h1>This is Header</h1>
+
+        <Image src={'https://links.papareact.com/ua6'} width={200} height={200}/>
+    </header>
+    );}
+
+export default Header;
