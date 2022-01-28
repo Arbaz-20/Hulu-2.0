616aff159efad3714dd70abf2859610b9dc85f41
Arbaz Khan <khanarbaz26393@gmail.com>
2022-01-28 19:01:27 +0530

    api thing is still remaining

--- Utility/requests.js
+++ Utility/requests.js
@@ -11,39 +11,39 @@ export default{
     },
     fetchActionMovies:{
         title:'Action',
-        url:`/discovery/movie?api_key=${API_KEY}&language=en-US`
+        url:`/movie/movie?api_key=${API_KEY}&with_genres=`
     },
     fetchComedyMovies:{
         title:'Comedy',
-        url:`/discovery/movie?api_key=${API_KEY}&with_genres=35`
+        url:`/movie/movieid?api_key=${API_KEY}&with_genres=35`
     },
     fetchHorrorMovies:{
         title:'Horror',
-        url:`/discovery/movie/?api_key=${API_KEY}&with_genres=27`
+        url:`/movie/movie?api_key=${API_KEY}&with_genres=27`
     },
     fetchRomanceMovies:{
         title:'Romance',
-        url:`/discovery/movie/?api_key=${API_KEY}&with_genres=10749`
+        url:`/movie/movie?api_key=${API_KEY}&with_genres=10749`
     },
     fetchMystery:{
         title:'Mystery',
-        url:`/discovery/movie/?api_key=${API_KEY}&with_genres=9648`
+        url:`/movie/movie?api_key=${API_KEY}&with_genres=9648`
     },
     fetchSciFi:{
         title:'Sci-Fi',
-        url:`/discovery/movie/?api_key=${API_KEY}&with_genres=878`
+        url:`/movie/movie?api_key=${API_KEY}&with_genres=878`
     },
     fetchWestern:{
         title:'Western',
-        url:`/discovery/movie/?api_key=${API_KEY}&with_genres=37`
+        url:`/movie/movie?api_key=${API_KEY}&with_genres=37`
     },
     fetchAnimation:{
         title:'Animation',
-        url:`/discovery/movie/?api_key=${API_KEY}&with_genres=16`
+        url:`/movie/movie?api_key=${API_KEY}&with_genres=16`
     },
     fetchTV:{
         title:'TV Movie',
-        url:`/discovery/movie/?api_key=${API_KEY}&with_genres=10770`
+        url:`/movie/movie?api_key=${API_KEY}&with_genres=10770`
     }
     
 }
